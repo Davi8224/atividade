bf02bc08c2829718f02d5e98460d472600cdd765
Davi8224 <davigoes178@gmail.com>
2026-09-16 19:37:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function coverter(){
     var valor = window.document.getElementById('numero')
     var num = Number(valor.value)
     var valorconvertido = num/5.15
-    document.getElementById("resultado").innerHTML = "O valor convertido é de " + valorconvertido + " dólares"
+    var total = valorconvertido.toFixed(2)
+    document.getElementById("resultado").innerHTML = "O valor convertido é de " + total + " dólares"
 }
