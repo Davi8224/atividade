ff06c72af6315f886f2fd46dee922474c0a22f11
Davi8224 <davigoes178@gmail.com>
2026-09-22 16:19:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ function coverter(){
     var valor = window.document.getElementById('numero')
     var num = Number(valor.value)
     var valorconvertido = num/5.15
-    var total = valorconvertido.toFixed(2)
-    document.getElementById("resultado").innerHTML = "O valor convertido é de " + total + " dólares"
+    const numero = valorconvertido.toFixed(2);
+    document.getElementById("resultado").innerHTML = "O valor convertido é de " + numero + " dólares"
 }
+
+let darkmode = localStorage.getItem('darkmode')
+const mudar = document.getElementById('mudar')
+const modoescuro = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');    
+}
+
+const modoclaro = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);    
+}
+
+if(darkmode === "active") modoescuro()
+
+mudar.addEventListener("click", () =>{
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? modoescuro() : modoclaro()
+})
